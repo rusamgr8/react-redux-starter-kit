ecc180ce46f45dd4747945a024b5b7a2bc7b6ef3
lab3 <lab3@tcslab.com>
2016-08-08 14:01:21 +0530

    first day

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 /* eslint-disable no-console */
-console.log('hi');
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './searchBar';
+
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App1 />, document.getElementById('app1'));
